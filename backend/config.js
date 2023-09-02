d5ed2da01f0e4b1abddbe6162b159681edc0b937
Daksh Singh <dakshsingh.1612@gmail.com>
2023-09-02 14:56:31 +0530

    Changes config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
 export const PORT = 8899;
 
-export const mongoDBURL =
-  "mongodb+srv://daksh:sOThgfJVSyv4SlAH@bookstore-mern.gxljiz1.mongodb.net/books-collection?retryWrites=true&w=majority";
+export const mongoDBURL = "";
